02ceea9c3cd6ae748a14bc333b8c3b51fa2a5c52
Andrew Tan <andrewtsoc@gmail.com>
2015-02-01 01:17:02 -0500

    Handling rare exception in ResultParser

--- MyApplication/mobile/src/main/java/yaning/uofthacks15/ResultParser.java
+++ MyApplication/mobile/src/main/java/yaning/uofthacks15/ResultParser.java
@@ -11,25 +11,29 @@ import java.util.Iterator;
 
 public class ResultParser {
     public static String parse(String html) {
-        Document doc = Jsoup.parse(html);
-        Elements elems = doc.select("em"); // get bolded elements
+        try {
+            Document doc = Jsoup.parse(html);
+            Elements elems = doc.select("em"); // get bolded elements
 
-        ArrayList<String> ret = new ArrayList<String>(); // to store element data
-        ArrayList<Integer> freq = new ArrayList<Integer>();
+            ArrayList<String> ret = new ArrayList<String>(); // to store element data
+            ArrayList<Integer> freq = new ArrayList<Integer>();
 
-        for (Iterator<Element> i = elems.iterator(); i.hasNext();){
-            Element e = i.next();
-            ret.add(e.html().toLowerCase());
+            for (Iterator<Element> i = elems.iterator(); i.hasNext(); ) {
+                Element e = i.next();
+                ret.add(e.html().toLowerCase());
 
 
-            System.out.println(e.html()); // debug
+                System.out.println(e.html()); // debug
+            }
+            // get most frequent
+            for (String s : ret)
+                freq.add(Collections.frequency(ret, s));
+            int max_ind = freq.indexOf(Collections.max(freq));
+
+            return ret.get(max_ind);
+        } catch (Exception e) {
+            return "";
         }
-        // get most frequent
-        for (String s: ret)
-            freq.add(Collections.frequency(ret, s));
-        int max_ind = freq.indexOf(Collections.max(freq));
-
-        return ret.get(max_ind);
     }
 
 }
